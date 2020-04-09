7c024c200475ae83d8ba21b5754a31bdfcccbd7b
AlexHodovas <o.hodovas@gmail.com>
2020-04-09 13:37:54 +0300

    Updates

---
+++ precache-manifest.7b24108cae097e5d6b36617bd7ff21f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1d3c035dc3b868a619e3b3c628891b9",
+    "url": "/blocks_react/index.html"
+  },
+  {
+    "revision": "413b4f306142f7b249b4",
+    "url": "/blocks_react/static/css/main.58e7daca.chunk.css"
+  },
+  {
+    "revision": "b6ee1548119b5b95d0b2",
+    "url": "/blocks_react/static/js/2.3ec2381d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/blocks_react/static/js/2.3ec2381d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "413b4f306142f7b249b4",
+    "url": "/blocks_react/static/js/main.1864c4ed.chunk.js"
+  },
+  {
+    "revision": "3451e2db89e6fa5fd162",
+    "url": "/blocks_react/static/js/runtime-main.95866655.js"
+  }
+]);
